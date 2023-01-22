0298a11fcea71d9bef98c2b956db85c192b69786
AdiLewin <adi.lewin.42@gmail.com>
2023-01-22 14:43:41 +0200

    great graphs!!

--- create_dists_probabilities.py
+++ create_dists_probabilities.py
@@ -1,20 +1,29 @@
 import numpy as np
 import pandas as pd
+import plotly.express as px
 
 
 def plot_all():
     locations = np.load("locations.npy")
     amount_in_pair = np.load("amount_in_pair.npy")
-    amount_in_pair = amount_in_pair / amount_in_pair.sum(axis=1)[:, None]
-    dists = locations[1:] - locations[:-1]  # TODO: missing one line
-    merged_data = np.hstack([dists.reshape(dists.shape[0], 1), amount_in_pair[1:]])
 
-    df = pd.DataFrame(merged_data)
-    df.columns = ['dist', '0->0', '0->1', '1->1', "1->0"]
-    df = df.groupby("dist").sum()
+    dists = locations[1:] - locations[:-1]  # TODO: missing one line
+    merged_data = np.hstack([dists.reshape(dists.shape[0], 1)[:-1], amount_in_pair[1:-1]])  # TODO: there are nulls
+    df = pd.DataFrame(merged_data).dropna()
+    df.columns = ['dist', '0->0', '0->1', '1->0', "1->1"]
+    df = df.groupby("dist", as_index=False).sum()
+    df = df[df["dist"] > 0].dropna()
+    prior_0 = (df['0->0'] + df['0->1'])
+    prior_1 = df['1->0'] + df['1->1']
+    df['0->0'] /= prior_0
+    df['0->1'] /= prior_0
+    df['1->0'] /= prior_1
+    df['1->1'] /= prior_1
+    df = df.dropna()
     print(df)
 
-
+    fig = px.scatter(df, x='dist', y=df.columns)
+    fig.show()
 
 
 if __name__ == '__main__':
